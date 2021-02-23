af08364f5f18159dbe34458d105bb416f2c9beb0
심성윤 <56909576+jghee717@users.noreply.github.com>
2021-02-24 01:57:56 +0900

    controller home order

--- src/main/java/jpabook/jpashop/repository/OrderSearch.java
+++ src/main/java/jpabook/jpashop/repository/OrderSearch.java
@@ -1,10 +1,12 @@
 package jpabook.jpashop.repository;
 
+import jpabook.jpashop.domain.OrderStatus;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter@Setter
 public class OrderSearch {
 
-    private String memberName;
+    private String memberName; // 회원이름
+    private OrderStatus orderStatus; //주문상태 order,cancle
 }
